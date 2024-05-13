abdc263f81df8971517319e4e37c19ef0f373902
abrambeazer <abrambeazer@gmail.com>
2024-05-13 10:20:31 -0700

    Added stubs for Volt Switch, U-Turn, and Baton Pass

---
+++ src/main/java/org/shorts/model/moves/UTurn.java
@@ -0,0 +1,12 @@
+package org.shorts.model.moves;
+
+import org.shorts.model.types.Type;
+
+public class UTurn extends PhysicalMove {
+
+    public UTurn() {
+        super("U-Turn", 70, 100, Type.BUG, 32, true, 100);
+    }
+
+    //TODO: Implement methods
+}
