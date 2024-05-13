dc7509c9da76566f42e4c5869ae74df678369702
abrambeazer <abrambeazer@gmail.com>
2024-05-13 10:20:51 -0700

    Added Justified ability

---
+++ src/main/java/org/shorts/model/abilities/Justified.java
@@ -0,0 +1,23 @@
+package org.shorts.model.abilities;
+
+import org.shorts.battle.Battle;
+import org.shorts.model.moves.Move;
+import org.shorts.model.pokemon.Pokemon;
+import org.shorts.model.types.Type;
+
+public class Justified extends Ability {
+
+    private Justified() {
+        super("Justified");
+    }
+
+    public static final Justified JUSTIFIED = new Justified();
+
+    @Override
+    public void afterHit(Pokemon self, Pokemon opponent, Battle battle, int previousHP, Move move) {
+        //TODO: Make sure the logic for hitting/damaging/breaking a sub happens before this.
+        if (move.getType() == Type.DARK && !opponent.hasSubstitute()) {
+            self.changeAttack(1);
+        }
+    }
+}
