a91f9b51b1d13678a7106cfcaa1c843a17539239
abrambeazer <abrambeazer@gmail.com>
2024-05-13 18:16:54 -0700

    MagicBounce ability now confers the Magic Coat volatile status

--- src/main/java/org/shorts/model/abilities/MagicBounce.java
+++ src/main/java/org/shorts/model/abilities/MagicBounce.java
@@ -1,12 +1,27 @@
 package org.shorts.model.abilities;
 
+import org.shorts.battle.Battle;
+import org.shorts.model.pokemon.Pokemon;
+import org.shorts.model.status.VolatileStatus;
+import org.shorts.model.status.VolatileStatusType;
+
 public class MagicBounce extends Ability implements IgnorableAbility {
 
     private MagicBounce() {
         super("Magic Bounce");
     }
 
-    public static final MagicBounce MAGIC_BOUNCE = new org.shorts.model.abilities.MagicBounce();
+    public static final MagicBounce MAGIC_BOUNCE = new MagicBounce();
+
+    @Override
+    public void onGainAbility(Pokemon self) {
+        self.addVolatileStatus(VolatileStatus.MAGIC_COAT);
+    }
+
+    @Override
+    public void onLoseAbility(Pokemon self, Pokemon opponent, Battle battle) {
+        self.removeVolatileStatus(VolatileStatusType.MAGIC_COAT);
+    }
 }
 
 
