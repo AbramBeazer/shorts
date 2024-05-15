5808c47b7321ddaf435e8cc9a4f2cde5416a2a55
abrambeazer <abrambeazer@gmail.com>
2024-05-15 12:45:25 -0700

    Add Shed Shell held item

---
+++ src/main/java/org/shorts/model/items/ShedShell.java
@@ -0,0 +1,10 @@
+package org.shorts.model.items;
+
+public class ShedShell extends HeldItem {
+
+    private ShedShell() {
+        super("Shed Shell");
+    }
+
+    public static final ShedShell SHED_SHELL = new ShedShell();
+}
