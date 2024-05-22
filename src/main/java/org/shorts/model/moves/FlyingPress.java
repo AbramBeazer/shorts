9237457d0e44cc882ee34880ece2dc5f28ed178d
AbramBeazer <32072243+AbramBeazer@users.noreply.github.com>
2024-05-21 17:36:49 -0700

    Add tests for FlyingPress

--- src/main/java/org/shorts/model/moves/FlyingPress.java
+++ src/main/java/org/shorts/model/moves/FlyingPress.java
@@ -9,8 +9,8 @@ import static org.shorts.model.types.Type.FLYING;
 
 public class FlyingPress extends PhysicalMove {
 
-    private FlyingPress(Type moveType) {
-        super("Flying Press", 100, 100, moveType, 95, true, 0);
+    public FlyingPress() {
+        super("Flying Press", 100, 100, FIGHTING, 95, true, 0);
     }
 
     @Override
@@ -18,5 +18,4 @@ public class FlyingPress extends PhysicalMove {
         return super.getBaseTypeMultiplier(defenderTypes) * Type.getTypeMultiplier(FLYING, defenderTypes);
     }
 
-    public static final FlyingPress FLYING_PRESS = new FlyingPress(FIGHTING);
 }
