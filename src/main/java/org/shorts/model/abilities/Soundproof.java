35d490615619183e479ee0b794c2709ae05ae527
AbramBeazer <32072243+AbramBeazer@users.noreply.github.com>
2024-05-26 15:52:48 -0700

    Add Soundproof ability and interface for sound-based moves and abilities.

---
+++ src/main/java/org/shorts/model/abilities/Soundproof.java
@@ -0,0 +1,9 @@
+package org.shorts.model.abilities;
+
+public class Soundproof extends Ability {
+    private Soundproof() {
+        super("Soundproof");
+    }
+
+    public static final Soundproof SOUNDPROOF = new Soundproof();
+}
