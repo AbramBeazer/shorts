272d7157da8bb72e4bd792a9d9d15cafa11d0582
abrambeazer <abrambeazer@gmail.com>
2024-05-28 15:57:38 -0700

    Add Ingrain

--- src/main/java/org/shorts/model/moves/trapping/Ingrain.java
+++ src/main/java/org/shorts/model/moves/trapping/Ingrain.java
@@ -1,5 +1,21 @@
 package org.shorts.model.moves.trapping;
 
-public class Ingrain {
+import org.shorts.battle.Battle;
+import org.shorts.model.moves.Move;
+import org.shorts.model.moves.Range;
+import org.shorts.model.pokemon.Pokemon;
+import org.shorts.model.status.VolatileStatus;
+import org.shorts.model.status.VolatileStatusType;
+import org.shorts.model.types.Type;
 
+public class Ingrain extends Move {
+
+    public Ingrain() {
+        super("Ingrain", 0, -1, Type.GRASS, Category.STATUS, Range.SELF, 32, false, 100);
+    }
+
+    @Override
+    protected void applySecondaryEffect(Pokemon attacker, Pokemon defender, Battle battle) {
+        attacker.addVolatileStatus(new VolatileStatus(VolatileStatusType.ROOTED, -1));
+    }
 }
