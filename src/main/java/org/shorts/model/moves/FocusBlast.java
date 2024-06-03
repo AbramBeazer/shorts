6dd956fb0751b55b28548b2cbd15912475cd3403
abrambeazer <abrambeazer@gmail.com>
2024-06-03 14:47:33 -0700

    Add Egg Bomb, Electro Ball, Energy Ball, and Focus Miss.

---
+++ src/main/java/org/shorts/model/moves/FocusBlast.java
@@ -0,0 +1,25 @@
+package org.shorts.model.moves;
+
+import org.shorts.battle.Battle;
+import org.shorts.model.StatEnum;
+import org.shorts.model.pokemon.Pokemon;
+import org.shorts.model.types.Type;
+
+public class FocusBlast extends Move implements BallBombMove {
+
+    public FocusBlast() {
+        super("Focus Blast", 120, 70, Type.FIGHTING, Category.SPECIAL, Range.SINGLE_ADJACENT_ANY, 8, false, 10);
+    }
+
+    @Override
+    public void trySecondaryEffect(Pokemon user, Pokemon target, Battle battle) {
+        if (target.isDropPossible(StatEnum.SPDEF)) {
+            super.trySecondaryEffect(user, target, battle);
+        }
+    }
+
+    @Override
+    protected void applySecondaryEffect(Pokemon user, Pokemon target, Battle battle) {
+        target.changeSpecialDefense(-1);
+    }
+}
