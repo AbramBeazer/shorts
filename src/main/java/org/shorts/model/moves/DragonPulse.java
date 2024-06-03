bc3bb855304e0c3a212b754cb22dd60523994f9a
abrambeazer <abrambeazer@gmail.com>
2024-06-03 12:21:01 -0700

    Add pulse moves

---
+++ src/main/java/org/shorts/model/moves/DragonPulse.java
@@ -0,0 +1,10 @@
+package org.shorts.model.moves;
+
+import org.shorts.model.types.Type;
+
+public class DragonPulse extends Move implements PulseEffect {
+
+    public DragonPulse() {
+        super("Dragon Pulse", 85, 100, Type.DRAGON, Category.SPECIAL, Range.SINGLE_ANY, 16, false, 0);
+    }
+}
