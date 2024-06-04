6c5f014dc3b7f9e0df31b452fd14861ca9490564
abrambeazer <abrambeazer@gmail.com>
2024-06-04 16:05:18 -0700

    Add Pollen Puff and Telepathy ability (stub)

---
+++ src/main/java/org/shorts/model/abilities/Telepathy.java
@@ -0,0 +1,12 @@
+package org.shorts.model.abilities;
+
+public class Telepathy extends Ability {
+
+    private Telepathy() {
+        super("Telepathy");
+    }
+
+    public static final Telepathy TELEPATHY = new Telepathy();
+
+    //TODO: Implement
+}
