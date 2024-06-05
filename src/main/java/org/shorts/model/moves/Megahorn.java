ab232d9b4463797f7628536e7ece22d4a911f2d7
abrambeazer <abrambeazer@gmail.com>
2024-06-05 11:03:21 -0700

    Add Megahorn

---
+++ src/main/java/org/shorts/model/moves/Megahorn.java
@@ -0,0 +1,10 @@
+package org.shorts.model.moves;
+
+import org.shorts.model.types.Type;
+
+public class Megahorn extends Move {
+
+    public Megahorn() {
+        super("Megahorn", 120, 85, Type.BUG, Category.PHYSICAL, Range.SINGLE_ADJACENT_ANY, 16, true, 0);
+    }
+}
