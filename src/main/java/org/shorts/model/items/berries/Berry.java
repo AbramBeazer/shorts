3c8f2ec6e175287fb5e7b8fe3ebaba67bdf4d11e
abrambeazer <abrambeazer@gmail.com>
2024-06-05 15:30:56 -0700

    Add Teatime move and test stubs. Extract actual berry-eating logic from tryEatingBerry to eatBerry, because Teatime bypasses Unnerve and Magic Room.

--- src/main/java/org/shorts/model/items/berries/Berry.java
+++ src/main/java/org/shorts/model/items/berries/Berry.java
@@ -5,12 +5,12 @@ import org.shorts.model.abilities.CudChew;
 import org.shorts.model.abilities.OpponentCantEatBerriesAbility;
 import org.shorts.model.items.HeldItem;
 import org.shorts.model.pokemon.Pokemon;
-import org.shorts.model.status.VolatileStatusType;
 
 import static org.shorts.model.abilities.CheekPouch.CHEEK_POUCH;
 import static org.shorts.model.abilities.Gluttony.GLUTTONY;
 import static org.shorts.model.items.NoItem.NO_ITEM;
 import static org.shorts.model.status.VolatileStatusType.ABILITY_SUPPRESSED;
+import static org.shorts.model.status.VolatileStatusType.HEAL_BLOCKED;
 
 public abstract class Berry extends HeldItem {
 
@@ -40,24 +40,30 @@ public abstract class Berry extends HeldItem {
     }
 
     public boolean tryEatingBerry(Pokemon user, Battle battle) {
-        if (!(battle.getOpposingTrainer(user).getLead().getAbility() instanceof OpponentCantEatBerriesAbility)) {
+        Pokemon opponent = battle.getOpposingTrainer(user).getLead();
+        if (!(opponent.getAbility() instanceof OpponentCantEatBerriesAbility) || opponent.hasVolatileStatus(
+            ABILITY_SUPPRESSED)) {
 
-            this.doEffect(user);
-            System.out.println(user.getNickname() + " ate its " + this.getName());
-            user.setHeldItem(NO_ITEM);
-            user.setConsumedItem(this);
-
-            if (user.getAbility() == CHEEK_POUCH && !user.hasVolatileStatus(ABILITY_SUPPRESSED)
-                && !user.hasVolatileStatus(VolatileStatusType.HEAL_BLOCKED)) {
-
-                user.heal(user.getMaxHP() / 3);
-
-            } else if (user.getAbility() instanceof CudChew && !user.hasVolatileStatus(ABILITY_SUPPRESSED)) {
-                ((CudChew) user.getAbility()).setBerryToRegurgitate(this);
-            }
+            this.eatBerry(user);
             return true;
         }
         return false;
+    }
+
+    public void eatBerry(Pokemon user) {
+
+        this.doEffect(user);
+        System.out.println(user.getNickname() + " ate its " + this.getName());
+        user.setHeldItem(NO_ITEM);
+        user.setConsumedItem(this);
+
+        if (!user.hasVolatileStatus(ABILITY_SUPPRESSED)) {
+            if (user.getAbility() == CHEEK_POUCH && !user.hasVolatileStatus(HEAL_BLOCKED)) {
+                user.heal(user.getMaxHP() / 3);
+            } else if (user.getAbility() instanceof CudChew) {
+                ((CudChew) user.getAbility()).setBerryToRegurgitate(this);
+            }
+        }
     }
 
     public void doEffect(Pokemon user) {
---
+++ src/test/java/org/shorts/model/moves/TeatimeTests.java
@@ -0,0 +1,38 @@
+package org.shorts.model.moves;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TeatimeTests {
+
+    @Test
+    void testAllPokemonEatBerries() {
+        assertThat(false).isTrue();
+    }
+
+    @Test
+    void testBerryIsEatenEvenAboveThreshold() {
+        assertThat(false).isTrue();
+    }
+
+    @Test
+    void testBypassesSubstituteButNotSemiInvulnerable() {
+        assertThat(false).isTrue();
+    }
+
+    @Test
+    void testIgnoresUnnerve() {
+        assertThat(false).isTrue();
+    }
+
+    @Test
+    void testIgnoresMagicRoom() {
+        assertThat(false).isTrue();
+    }
+
+    @Test
+    void testElectrifiedTeatimeShouldBeAbsorbedByAbilityEvenWithNoBerry() {
+        assertThat(false).isTrue();
+    }
+}
