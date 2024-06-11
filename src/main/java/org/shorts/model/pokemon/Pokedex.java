a9546aeb9ca12b17103d2762a85e3feb25732e69
abrambeazer <abrambeazer@gmail.com>
2024-06-10 20:00:18 -0700

    Add stub for Pokedex class that will load Pokedex entries.

---
+++ src/main/java/org/shorts/model/pokemon/Pokedex.java
@@ -0,0 +1,13 @@
+package org.shorts.model.pokemon;
+
+import java.util.Map;
+
+public final class Pokedex {
+
+    private Map<String, PokedexEntry> dex;
+
+    static {
+        //TODO: Read from DB or Excel.
+
+    }
+}
