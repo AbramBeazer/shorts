65463af11e61ee06049be141eb48e9b03aab7556
abrambeazer <abrambeazer@gmail.com>
2024-06-20 11:22:04 -0700

    Add Big Root item and Liquid Ooze ability

---
+++ src/main/java/org/shorts/model/abilities/LiquidOoze.java
@@ -0,0 +1,10 @@
+package org.shorts.model.abilities;
+
+public class LiquidOoze extends Ability {
+
+    private LiquidOoze() {
+        super("Liquid Ooze");
+    }
+
+    public static final LiquidOoze LIQUID_OOZE = new LiquidOoze();
+}
---
+++ src/main/java/org/shorts/model/items/BigRoot.java
@@ -0,0 +1,10 @@
+package org.shorts.model.items;
+
+public class BigRoot extends HeldItem {
+
+    private BigRoot() {
+        super("Big Root");
+    }
+
+    public static final BigRoot BIG_ROOT = new BigRoot();
+}
