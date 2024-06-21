7f8ee8de5a2d017fd3b4f461729d77c966218505
abrambeazer <abrambeazer@gmail.com>
2024-06-21 16:33:09 -0700

    Add more test cases for VoltAbsorb and WaterAbsorb

--- src/test/java/org/shorts/model/abilities/elementabsorb/WaterAbsorbTests.java
+++ src/test/java/org/shorts/model/abilities/elementabsorb/WaterAbsorbTests.java
@@ -1,6 +1,66 @@
 package org.shorts.model.abilities.elementabsorb;
 
-class WaterAbsorbTests {
-    //TODO: Should water-type status moves like Soak trigger this ability? Thunder Wave will trigger Volt Absorb.
+import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.shorts.battle.Battle;
+import org.shorts.battle.DummySingleBattle;
+import org.shorts.model.moves.Move;
+import org.shorts.model.moves.Range;
+import org.shorts.model.moves.Surf;
+import org.shorts.model.moves.multihit.MultiHitMove;
+import org.shorts.model.pokemon.Pokemon;
+import org.shorts.model.pokemon.PokemonTestUtils;
+import org.shorts.model.types.Type;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WaterAbsorbTests {
+
+    private Pokemon user;
+    private Pokemon other;
+    private Battle battle;
+    private Move move;
+
+    @BeforeEach
+    void setup() {
+        move = new Surf();
+        user = PokemonTestUtils.getDummyPokemon();
+        other = PokemonTestUtils.getDummyPokemon();
+        battle = new DummySingleBattle(user, other);
+    }
+
+    //TODO: Should water-type status moves like Soak trigger this ability? Thunder Wave will trigger Volt Absorb.
+    //    @Test
+    //    void testAbsorbsWaterStatusMove(){
+    //
+    //    }
+
+    @Test
+    void testAbsorbsWaterAttack() {
+        assertThat(false).isTrue();
+    }
+
+    @Test
+    void testOnlyActivatedOnceByMultiHitMove() {
+        final int halfHealth = user.getMaxHP() / 2;
+        user.setCurrentHP(halfHealth);
+        move = new MultiHitMove(
+            "Test",
+            10,
+            100,
+            Type.WATER,
+            Move.Category.SPECIAL,
+            Range.SINGLE_ADJACENT_ANY,
+            24,
+            false,
+            0,
+            2,
+            2) {
+
+        };
+        move.execute(other, List.of(user), battle);
+        assertThat(user.getCurrentHP()).isEqualTo(user.getMaxHP() * 3 / 4);
+    }
 }
