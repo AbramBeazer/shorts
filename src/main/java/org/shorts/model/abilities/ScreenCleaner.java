dcb7e898363b1b76d9786ec14ae4e2a7aa28ca15
abrambeazer <abrambeazer@gmail.com>
2024-06-28 14:58:47 -0700

    Implement ScreenCleaner and add tests

--- src/main/java/org/shorts/model/abilities/ScreenCleaner.java
+++ src/main/java/org/shorts/model/abilities/ScreenCleaner.java
@@ -1,5 +1,8 @@
 package org.shorts.model.abilities;
 
+import org.shorts.battle.Battle;
+import org.shorts.model.pokemon.Pokemon;
+
 public class ScreenCleaner extends Ability {
 
     private ScreenCleaner() {
@@ -8,5 +11,14 @@ public class ScreenCleaner extends Ability {
 
     public static final ScreenCleaner SCREEN_CLEANER = new ScreenCleaner();
 
-    //TODO: Implement (afterEntry)
+    @Override
+    public void afterEntry(Pokemon self, Battle battle) {
+        battle.getPlayerOne().setAuroraVeilTurns(0);
+        battle.getPlayerOne().setReflectTurns(0);
+        battle.getPlayerOne().setLightScreenTurns(0);
+        
+        battle.getPlayerTwo().setAuroraVeilTurns(0);
+        battle.getPlayerTwo().setReflectTurns(0);
+        battle.getPlayerTwo().setLightScreenTurns(0);
+    }
 }
--- src/test/java/org/shorts/model/abilities/ScreenCleanerTests.java
+++ src/test/java/org/shorts/model/abilities/ScreenCleanerTests.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.shorts.battle.Battle;
 import org.shorts.battle.DummyBattle;
+import org.shorts.battle.Trainer;
 import org.shorts.model.pokemon.Pokemon;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.shorts.model.abilities.NeutralizingGas.NEUTRALIZING_GAS;
+import static org.shorts.model.abilities.ScreenCleaner.SCREEN_CLEANER;
 import static org.shorts.model.pokemon.PokemonTestUtils.getDummyPokemon;
 
 class ScreenCleanerTests {
@@ -17,16 +20,57 @@ class ScreenCleanerTests {
     @BeforeEach
     void setUp() {
         user = getDummyPokemon();
+        user.setAbility(SCREEN_CLEANER);
         battle = new DummyBattle(user, getDummyPokemon());
     }
 
     @Test
     void testRemovesScreensOnEntry() {
-        assertThat(false).isTrue();
+        final Trainer player = battle.getCorrespondingTrainer(user);
+        final Trainer opponent = battle.getOpposingTrainer(user);
+
+        player.setAuroraVeilTurns(5);
+        player.setReflectTurns(5);
+        player.setLightScreenTurns(5);
+
+        opponent.setAuroraVeilTurns(5);
+        opponent.setReflectTurns(5);
+        opponent.setLightScreenTurns(5);
+
+        user.afterEntry(battle);
+
+        assertThat(player.getAuroraVeilTurns()).isZero();
+        assertThat(player.getReflectTurns()).isZero();
+        assertThat(player.getLightScreenTurns()).isZero();
+
+        assertThat(opponent.getAuroraVeilTurns()).isZero();
+        assertThat(opponent.getReflectTurns()).isZero();
+        assertThat(opponent.getLightScreenTurns()).isZero();
     }
 
     @Test
     void testAbilityDoesNotActivateOnEntryIfNeutralizingGasIsPresent() {
-        assertThat(false).isTrue();
+        final Trainer player = battle.getCorrespondingTrainer(user);
+        final Trainer opponent = battle.getOpposingTrainer(user);
+
+        player.setAuroraVeilTurns(5);
+        player.setReflectTurns(5);
+        player.setLightScreenTurns(5);
+
+        opponent.setAuroraVeilTurns(5);
+        opponent.setReflectTurns(5);
+        opponent.setLightScreenTurns(5);
+
+        opponent.getActivePokemon().get(0).setAbility(NEUTRALIZING_GAS);
+        opponent.getActivePokemon().get(0).afterEntry(battle);
+        user.afterEntry(battle);
+
+        assertThat(player.getAuroraVeilTurns()).isEqualTo(5);
+        assertThat(player.getReflectTurns()).isEqualTo(5);
+        assertThat(player.getLightScreenTurns()).isEqualTo(5);
+
+        assertThat(opponent.getAuroraVeilTurns()).isEqualTo(5);
+        assertThat(opponent.getReflectTurns()).isEqualTo(5);
+        assertThat(opponent.getLightScreenTurns()).isEqualTo(5);
     }
 }
