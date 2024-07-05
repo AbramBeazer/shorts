212c7b5d432d49fd405ac934213baf6f5d1ae193
abrambeazer <abrambeazer@gmail.com>
2024-07-05 13:49:38 -0700

    Add Growl, Screech, and Corrosive Gas

---
+++ src/main/java/org/shorts/model/moves/Screech.java
@@ -0,0 +1,26 @@
+package org.shorts.model.moves;
+
+import org.shorts.battle.Battle;
+import org.shorts.model.StatEnum;
+import org.shorts.model.pokemon.Pokemon;
+import org.shorts.model.types.Type;
+
+public class Screech extends Move implements AffectedByMagicBounce, SoundEffect {
+
+    public Screech() {
+        super("Screech", 0, 85, Type.NORMAL, Category.STATUS, Range.SINGLE_ADJACENT_ANY, 64, false, 100);
+    }
+
+    @Override
+    public void trySecondaryEffect(Pokemon user, Pokemon target, Battle battle) {
+        if (target.isDropPossible(StatEnum.DEF) && !SoundEffect.super.soundproofApplies(target)) {
+            super.trySecondaryEffect(user, target, battle);
+        }
+    }
+
+    @Override
+    protected void applySecondaryEffect(Pokemon user, Pokemon target, Battle battle) {
+        target.changeStat(-2, StatEnum.DEF);
+    }
+
+}
