6409149564a133d588f1b9699e1ae6da5946ddfe
abrambeazer <abrambeazer@gmail.com>
2024-08-20 15:01:19 -0700

    fix bug where attacker was eating target's type-resist berry

--- src/main/java/org/shorts/model/moves/Move.java
+++ src/main/java/org/shorts/model/moves/Move.java
@@ -743,7 +743,7 @@ public abstract class Move {
         if (target.getHeldItem() instanceof TypeResistBerry) {
             TypeResistBerry berry = (TypeResistBerry) target.getHeldItem();
             if (berry.getType() == this.type && (this.type == NORMAL || typeMultiplier > NEUTRAL)) {
-                final boolean ateBerry = berry.tryEatingBerry(user, battle);
+                final boolean ateBerry = berry.tryEatingBerry(target, battle);
                 if (ateBerry) {
                     target.setHeldItem(NO_ITEM);
                     target.setConsumedItem(berry);
