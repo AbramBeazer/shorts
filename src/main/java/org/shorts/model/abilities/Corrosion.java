5bb1c30911fcfee9689f5d889dde9c02be05ac0f
abrambeazer <abrambeazer@gmail.com>
2024-11-18 15:28:56 -0800

    add Corrosion ability and test stubs

---
+++ src/main/java/org/shorts/model/abilities/Corrosion.java
@@ -0,0 +1,10 @@
+package org.shorts.model.abilities;
+
+public class Corrosion extends Ability {
+
+    private Corrosion() {
+        super("Corrosion");
+    }
+
+    public static final Corrosion CORROSION = new Corrosion();
+}
---
+++ src/test/java/org/shorts/model/abilities/CorrosionTests.java
@@ -0,0 +1,19 @@
+package org.shorts.model.abilities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CorrosionTests {
+
+    @Test
+    void testSteelTypesCanBePoisoned() {
+        assertThat(false).isTrue();
+    }
+
+    @Test
+    void testPoisonTypesCanBePoisoned() {
+        assertThat(false).isTrue();
+    }
+
+}
