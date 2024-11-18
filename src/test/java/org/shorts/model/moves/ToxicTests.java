ba1992d2175584476273895a6401a1257849e48e
abrambeazer <abrambeazer@gmail.com>
2024-11-18 15:22:50 -0800

    stub tests for Toxic move and status

---
+++ src/test/java/org/shorts/model/moves/ToxicTests.java
@@ -0,0 +1,18 @@
+package org.shorts.model.moves;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class ToxicTests {
+
+    @Test
+    void testPoisonTypesCannotMiss() {
+        assertThat(false).isTrue();
+    }
+
+    @Test
+    void testPoisonTypesHitThroughSemiInvulnerable() {
+        assertThat(false).isTrue();
+    }
+}
---
+++ src/test/java/org/shorts/model/status/ToxicStatusTests.java
@@ -0,0 +1,19 @@
+package org.shorts.model.status;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ToxicStatusTests {
+
+    @Test
+    void testCounterResetsOnEntry() {
+        assertThat(false).isTrue();
+    }
+
+    @Test
+    void testDamageIncreasesEachTurn() {
+        assertThat(false).isTrue();
+    }
+
+}
