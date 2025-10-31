e6088c1e36778b0631bf6f1051f1586a9d6df818
abrambeazer <abrambeazer@gmail.com>
2025-10-31 12:42:21 -0700

    DoubleIronBash should implement PunchingMove.

--- src/main/java/org/shorts/model/moves/multihit/fixed/DoubleIronBash.java
+++ src/main/java/org/shorts/model/moves/multihit/fixed/DoubleIronBash.java
@@ -1,10 +1,11 @@
 package org.shorts.model.moves.multihit.fixed;
 
 import org.shorts.model.moves.GetsSheerForceBoost;
+import org.shorts.model.moves.PunchingMove;
 import org.shorts.model.moves.Range;
 import org.shorts.model.types.Type;
 
-public class DoubleIronBash extends FixedMultiHitMove implements GetsSheerForceBoost {
+public class DoubleIronBash extends FixedMultiHitMove implements GetsSheerForceBoost, PunchingMove {
 
     public DoubleIronBash() {
         super("Double Iron Bash", 60, 100, Type.STEEL, Category.PHYSICAL, Range.NORMAL, 8, true, 0, 2);
