454c6effe947f41516309e09e0bc22d65ee25eeb
abrambeazer <abrambeazer@gmail.com>
2025-10-31 10:47:23 -0700

    add PunchingMove marker interface and IronFist ability

---
+++ src/main/java/org/shorts/model/abilities/IronFist.java
@@ -0,0 +1,23 @@
+package org.shorts.model.abilities;
+
+import org.shorts.battle.Battle;
+import org.shorts.model.moves.Move;
+import org.shorts.model.moves.punching.PunchingMove;
+import org.shorts.model.pokemon.Pokemon;
+
+public class IronFist extends Ability {
+
+    private IronFist() {
+        super("IronFist");
+    }
+
+    public static final IronFist IRON_FIST = new IronFist();
+
+    public static final double MULTIPLIER = 4915d / 4096d;
+
+    @Override
+    public double getMovePowerMultipliers(Pokemon user, Pokemon opponent, Battle battle, Move move) {
+        final double base = super.getMovePowerMultipliers(user, opponent, battle, move);
+        return move instanceof PunchingMove ? base * MULTIPLIER : base;
+    }
+}
---
+++ src/test/java/org/shorts/model/abilities/IronFistTests.java
@@ -0,0 +1,40 @@
+package org.shorts.model.abilities;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.shorts.battle.Battle;
+import org.shorts.battle.DummyBattle;
+import org.shorts.model.moves.Move;
+import org.shorts.model.moves.Tackle;
+import org.shorts.model.moves.multihit.CometPunch;
+import org.shorts.model.pokemon.Pokemon;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.shorts.model.pokemon.PokemonTestUtils.*;
+
+class IronFistTests {
+
+    private Pokemon user;
+    private Pokemon target;
+    private Battle battle;
+
+    @BeforeEach
+    void setUp() {
+        user = getDummyPokemon();
+        target = getDummyPokemon();
+        battle = new DummyBattle(user, target);
+    }
+
+    @Test
+    void testPowerBoosted() {
+        final double withPunchingMove = IronFist.IRON_FIST.getMovePowerMultipliers(
+            user,
+            target,
+            battle,
+            new CometPunch());
+        final double noPunching = IronFist.IRON_FIST.getMovePowerMultipliers(user, target, battle, new Tackle());
+        assertThat(withPunchingMove).isEqualTo(noPunching * IronFist.MULTIPLIER);
+    }
+}
