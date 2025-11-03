f9084edac526f47632c4c9a93dc2447184fe0cdb
abrambeazer <abrambeazer@gmail.com>
2025-11-03 08:36:00 -0800

    Add marker interface for kicking moves (so far, I don't think there's anything these moves mechanically have in common, but this may be nice to have someday.)

---
+++ src/main/java/org/shorts/model/moves/KickingMove.java
@@ -0,0 +1,6 @@
+package org.shorts.model.moves;
+
+//Unlike punching moves that have Iron Fist and Punching Glove, there's no ability or item that affects kicking moves, but this may be nice to have someday.
+public interface KickingMove {
+
+}
