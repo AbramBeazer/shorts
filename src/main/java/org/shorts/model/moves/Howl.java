042c7c895f2920cde875597ae1e2c36b97327819
abrambeazer <abrambeazer@gmail.com>
2025-11-17 13:19:16 -0800

    add Howl

---
+++ src/main/java/org/shorts/model/moves/Howl.java
@@ -0,0 +1,46 @@
+package org.shorts.model.moves;
+
+import org.shorts.battle.Battle;
+import org.shorts.model.StatEnum;
+import org.shorts.model.pokemon.Pokemon;
+import org.shorts.model.status.VolatileStatusType;
+import org.shorts.model.types.Type;
+
+import static org.shorts.model.abilities.Infiltrator.*;
+import static org.shorts.model.abilities.Soundproof.*;
+
+public class Howl extends Move implements SoundEffect {
+
+    public Howl() {
+        super("Howl", 0, -1, Type.NORMAL, Category.STATUS, Range.OWN_PARTY, 64, false, 100);
+    }
+
+    @Override
+    public void trySecondaryEffect(Pokemon user, Pokemon target, Battle battle) {
+        if (affectsTarget(user, target)) {
+            super.trySecondaryEffect(user, target, battle);
+        }
+    }
+
+    @Override
+    protected void applySecondaryEffect(Pokemon user, Pokemon target, Battle battle) {
+        target.changeStat(1, StatEnum.ATK);
+    }
+
+    protected boolean affectsTarget(Pokemon user, Pokemon target) {
+        boolean canAffect = true;
+        if (user != target) {
+            if (target.hasVolatileStatus(VolatileStatusType.SUBSTITUTE)) {
+                canAffect =
+                    user.getAbility() == INFILTRATOR && !user.hasVolatileStatus(VolatileStatusType.ABILITY_SUPPRESSED)
+                        && target.canChangeStat(1, StatEnum.ATK);
+            } else if (target.getAbility() == SOUNDPROOF) {
+                canAffect =
+                    !target.hasVolatileStatus(VolatileStatusType.ABILITY_SUPPRESSED) && !target.hasVolatileStatus(
+                        VolatileStatusType.ABILITY_IGNORED);
+            }
+        }
+
+        return canAffect && target.canChangeStat(1, StatEnum.ATK);
+    }
+}
