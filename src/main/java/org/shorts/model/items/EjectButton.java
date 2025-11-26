913a2273f3b104577c8ee00de53c3d5595f02c1a
abrambeazer <abrambeazer@gmail.com>
2025-11-26 13:41:53 -0800

    add stubs for EjectButton and RedCard.

---
+++ src/main/java/org/shorts/model/items/EjectButton.java
@@ -0,0 +1,12 @@
+package org.shorts.model.items;
+
+public class EjectButton extends HeldItem {
+
+    private EjectButton() {
+        super("Eject Button", 30);
+    }
+
+    public static final EjectButton EJECT_BUTTON = new EjectButton();
+
+    //TODO: Implement
+}
---
+++ src/main/java/org/shorts/model/items/RedCard.java
@@ -0,0 +1,12 @@
+package org.shorts.model.items;
+
+public class RedCard extends HeldItem {
+
+    private RedCard() {
+        super("Red Card", 10);
+    }
+
+    public static final RedCard RED_CARD = new RedCard();
+
+    //TODO: Implement
+}
