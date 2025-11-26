03b32da17c68722d9fd43c7e7ad334c5b2498b19
abrambeazer <abrambeazer@gmail.com>
2025-11-26 13:42:19 -0800

    pushing initial rework of SwitchSelfMove

--- src/main/java/org/shorts/model/moves/switchself/SwitchSelfMove.java
+++ src/main/java/org/shorts/model/moves/switchself/SwitchSelfMove.java
@@ -1,5 +1,12 @@
 package org.shorts.model.moves.switchself;
 
-public interface SwitchSelfMove {
+import org.shorts.battle.Battle;
+import org.shorts.model.moves.IMove;
+import org.shorts.model.pokemon.Pokemon;
 
+public interface SwitchSelfMove implements IMove {
+
+    boolean canSwitchSelf(Pokemon user, Pokemon target, Battle battle);
+
+    void switchSelf();
 }
--- src/main/java/org/shorts/model/moves/switchself/VoltSwitch.java
+++ src/main/java/org/shorts/model/moves/switchself/VoltSwitch.java
@@ -1,31 +1,42 @@
 package org.shorts.model.moves.switchself;
 
-//public class VoltSwitch extends Move implements SwitchSelfMove {
-//
-//    public VoltSwitch() {
-//        super("Volt Switch", 70, 100, Type.ELECTRIC, 32, false, 100);
-//    }
-//
-//    @Override
-//    public void trySecondaryEffect(Pokemon attacker, Pokemon defender, Battle battle) {
-//        final Trainer attackingTrainer =
-//            battle.getPlayerOne().getLead() == attacker ? battle.getPlayerOne() : battle.getPlayerTwo();
-//        final Trainer defendingTrainer =
-//            attackingTrainer == battle.getPlayerOne() ? battle.getPlayerTwo() : battle.getPlayerOne();
-//
-//        if (attackingTrainer.hasAvailableSwitch() && !defendingTrainer.hasLost()
-//            //TODO:
-//            //&& !triggers defender's Wimp Out
-//            //&& !triggers defender's Emergency Exit
-//            //&& Volt Switch will not force a switch if the user is holding a Red Card or the target is holding an Eject Button.
-//        ) {
-//            super.trySecondaryEffect(attacker, defender, battle);
-//        }
-//    }
-//
-//    @Override
-//    protected void applySecondaryEffect(Pokemon attacker, Pokemon defender, Battle battle) {
-//        //TODO: Figure out how to make the switch happen here.
-//        //TODO: Make sure this works with Pursuit.
-//    }
-//}
+import org.shorts.battle.Battle;
+import org.shorts.battle.Trainer;
+import org.shorts.model.moves.Move;
+import org.shorts.model.moves.Range;
+import org.shorts.model.pokemon.Pokemon;
+import org.shorts.model.types.Type;
+
+import static org.shorts.model.items.EjectButton.*;
+import static org.shorts.model.items.RedCard.*;
+
+public class VoltSwitch extends Move implements SwitchSelfMove {
+
+    public VoltSwitch() {
+        super("Volt Switch", 70, 100, Type.ELECTRIC, Category.SPECIAL, Range.NORMAL, 32, false, 100);
+    }
+
+    @Override
+    public boolean canSwitchSelf(Pokemon user, Pokemon target, Battle battle) {
+        //&& !triggers target's Wimp Out
+        //&& !triggers target's Emergency Exit
+        return user.getHeldItem() != RED_CARD && target.getHeldItem() != EJECT_BUTTON;
+    }
+
+    @Override
+    public void trySecondaryEffect(Pokemon user, Pokemon target, Battle battle) {
+        final Trainer attackingTrainer = battle.getCorrespondingTrainer(user);
+        final Trainer defendingTrainer = battle.getCorrespondingTrainer(target);
+
+        if (attackingTrainer.hasAvailableSwitch() && !defendingTrainer.hasLost()
+            && canSwitchSelf(user, target, battle)) {
+            super.trySecondaryEffect(user, target, battle);
+        }
+    }
+
+    @Override
+    protected void applySecondaryEffect(Pokemon attacker, Pokemon defender, Battle battle) {
+        //TODO: Figure out how to make the switch happen here.
+        //TODO: Make sure this works with Pursuit.
+    }
+}
