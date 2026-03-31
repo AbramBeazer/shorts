5d6da890d5c5dace47984bc464d85fd536bc8bc4
abrambeazer <abrambeazer@gmail.com>
2026-03-31 16:25:33 -0700

    add constants to Gyro Ball

--- src/main/java/org/shorts/model/moves/GyroBall.java
+++ src/main/java/org/shorts/model/moves/GyroBall.java
@@ -6,13 +6,16 @@ import org.shorts.model.types.Type;
 
 public class GyroBall extends Move implements BallBombMove, SpeedComparisonMove {
 
+    protected static final int BASE_POWER = 25;
+    protected static final int MAX_POWER = 150;
+
     public GyroBall() {
-        super("Gyro Ball", 25, 100, Type.STEEL, Category.PHYSICAL, Range.NORMAL, 8, true, 0);
+        super("Gyro Ball", BASE_POWER, 100, Type.STEEL, Category.PHYSICAL, Range.NORMAL, 8, true, 0);
     }
 
     @Override
     public double getPower(Pokemon user, Pokemon target, Battle battle) {
-        final double gyroBallPower = (25d * target.calculateSpeed(battle) / user.calculateSpeed(battle)) + 1;
-        return Math.min(150, gyroBallPower);
+        final double gyroBallPower = (BASE_POWER * target.calculateSpeed(battle) / user.calculateSpeed(battle)) + 1;
+        return Math.min(MAX_POWER, gyroBallPower);
     }
 }
