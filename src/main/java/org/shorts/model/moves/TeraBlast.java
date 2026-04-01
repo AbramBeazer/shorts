52335298e5b997ea8b3a2bac59873ad952b5954f
abrambeazer <abrambeazer@gmail.com>
2026-03-31 17:24:18 -0700

    Add tests for Tera Blast (stubs remain for logic around Galvanize and Gale Wings).

--- src/main/java/org/shorts/model/moves/TeraBlast.java
+++ src/main/java/org/shorts/model/moves/TeraBlast.java
@@ -7,8 +7,27 @@ import org.shorts.model.types.Type;
 
 public class TeraBlast extends Move {
 
+    protected static final int BASE_POWER = 80;
+    protected static final int STELLAR_POWER = 100;
+
     public TeraBlast() {
-        super("Tera Blast", 80, 100, Type.NORMAL, Category.SPECIAL, Range.NORMAL, 16, false, 0);
+        super("Tera Blast", BASE_POWER, 100, Type.NORMAL, Category.SPECIAL, Range.NORMAL, 16, false, 100);
+    }
+
+    @Override
+    public double getPower(Pokemon user, Pokemon target, Battle battle) {
+        return user.isTera() && user.getTeraType() instanceof Type.StellarType ? STELLAR_POWER : BASE_POWER;
+    }
+
+    @Override
+    protected int calculateDamage(Pokemon user, Pokemon target, Battle battle) {
+        if (user.isTera()
+            && user.calculateAttackWithoutAbilityOrItem() > user.calculateSpecialAttackWithoutAbilityOrItem()) {
+            setCategory(Category.PHYSICAL);
+        } else {
+            setCategory(Category.SPECIAL);
+        }
+        return super.calculateDamage(user, target, battle);
     }
 
     @Override
