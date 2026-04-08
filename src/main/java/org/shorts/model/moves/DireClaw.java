011ab030d16f0083e770abf9eac1ade2fbbe71fe
abrambeazer <abrambeazer@gmail.com>
2026-04-08 16:21:35 -0700

    rework Dire Claw so it rolls once with a 50% chance to activate (this is boosted by Serene Grace) and then rolls for which status gets inflicted.

--- src/main/java/org/shorts/model/moves/DireClaw.java
+++ src/main/java/org/shorts/model/moves/DireClaw.java
@@ -8,34 +8,27 @@ import org.shorts.model.types.Type;
 
 public class DireClaw extends Move implements GetsSheerForceBoost {
 
-    private Status currentStatus;
-
     public DireClaw() {
-        super("Dire Claw", 80, 100, Type.POISON, Category.PHYSICAL, Range.NORMAL, 24, true, 100);
-    }
-
-    @Override
-    public void trySecondaryEffect(Pokemon user, Pokemon target, Battle battle) {
-        final int rand = Main.RANDOM.nextInt(6);
-        if (rand == 0) {
-            currentStatus = Status.POISON;
-        } else if (rand == 1) {
-            currentStatus = Status.PARALYZE;
-        } else if (rand == 2) {
-            currentStatus = Status.createSleep();
-        }
-
-        if (currentStatus != null && currentStatus.getType().isStatusPossible(user, target, battle)) {
-            super.trySecondaryEffect(user, target, battle);
-        }
+        super("Dire Claw", 80, 100, Type.POISON, Category.PHYSICAL, Range.NORMAL, 24, true, 50);
     }
 
     //TODO: Is any of this right? If it rolls poison on a target that's already poisoned, does it then just paralyze it instead?
 
     @Override
     protected void applySecondaryEffect(Pokemon user, Pokemon target, Battle battle) {
-        target.setStatus(currentStatus);
-        target.afterStatus(user, battle);
-        currentStatus = null;
+        final Status currentStatus;
+
+        final int rand = Main.RANDOM.nextInt(3);
+        if (rand == 0) {
+            currentStatus = Status.POISON;
+        } else if (rand == 1) {
+            currentStatus = Status.PARALYZE;
+        } else {
+            currentStatus = Status.createSleep();
+        }
+        if (currentStatus.getType().isStatusPossible(user, target, battle)) {
+            target.setStatus(currentStatus);
+            target.afterStatus(user, battle);
+        }
     }
 }
