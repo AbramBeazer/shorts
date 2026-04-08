34af2852603f5a44c716c19e6232d62c7dc60a46
abrambeazer <abrambeazer@gmail.com>
2026-04-08 16:34:08 -0700

    Shadow Claw is now a slicing move.

--- src/main/java/org/shorts/model/moves/ShadowClaw.java
+++ src/main/java/org/shorts/model/moves/ShadowClaw.java
@@ -2,7 +2,7 @@ package org.shorts.model.moves;
 
 import org.shorts.model.types.Type;
 
-public class ShadowClaw extends Move implements HighCritChanceMove {
+public class ShadowClaw extends Move implements HighCritChanceMove, SlicingMove {
 
     public ShadowClaw() {
         super("Shadow Claw", 70, 100, Type.GHOST, Category.PHYSICAL, Range.NORMAL, 24, true, 0);
